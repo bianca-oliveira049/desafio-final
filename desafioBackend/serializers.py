d789e2c4132c8fd7642232700967aa7a778bebcc
bianca-oliveira049 <bianca.oliveira@pditabira.com.br>
2026-03-12 11:22:42 -0300

    Criação dos Serializers

---
+++ desafioBackend/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from desafioBackend.models import Aluno, Curso, Matricula
+
+class AlunoSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = '__all__'
+
+class CursoSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
+
+class MatriculaSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        fields = '__all__'
