0fd5c549b42cc72132dd683953f04d30e18c444f
bianca-oliveira049 <bianca.oliveira@pditabira.com.br>
2026-03-11 21:48:53 -0300

    Criação de models

--- desafioBackend/models.py
+++ desafioBackend/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+'''
+Tabela alunos: id, nome, email
+Tabela cursos: id, titulo
+Tabela matriculas: id, aluno_id, curso_id 
+'''
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=40, blank=False)
+
+    def __str__(self): return self.nome
+
+class Curso(models.Model):
+    titulo = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return self.titulo
+    
+class Matricula(models.Model):
+    id_aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
