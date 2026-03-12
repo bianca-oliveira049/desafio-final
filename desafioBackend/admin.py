bd21cadc70abfba96cea558a1e4e62090bbbf10d
bianca-oliveira049 <bianca.oliveira@pditabira.com.br>
2026-03-12 10:56:14 -0300

    Modificações em admin.py para adição dos models

--- desafioBackend/admin.py
+++ desafioBackend/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from desafioBackend.models import Aluno, Curso, Matricula
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ("id", "nome", "email")
+    list_display_links = ("id", "nome")
+    list_per_page = 20
+    search_fields = ("id",)
+
+admin.site.register(Aluno, Alunos)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ("id", "titulo")
+    list_display_links = ("id", "titulo")
+    list_per_page = 30
+    search_fields = ("id",)
+
+admin.site.register(Curso, Cursos)
+
+class Matriculas(admin.ModelAdmin):
+    list_display = ("id", "id_aluno", "id_curso")
+    list_display_links = ("id", "id_aluno", "id_curso")
+    list_per_page = 10
+    search_fields = ("id",)
+
+admin.site.register(Matricula, Matriculas)
